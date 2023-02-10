972c3753c702b6bf3dda9dd08d6fa86c5a40a948
derikvila1 <83841517+derikvila1@users.noreply.github.com>
2023-02-10 14:21:30 -0400

    semantic

--- app_controle_tarefas/resources/views/tarefa/index.blade.php
+++ app_controle_tarefas/resources/views/tarefa/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" type="text/css" href="/semantic/semantic.min.css">
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="/semantic/semantic.min.js"></script>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
